bdc198a0979ff3e2c70babff4a71c39e020a2ead
Scott Wei <leescott@microsoft.com>
2025-05-30 17:10:30 +0800

    Temp

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+export type Procedure<Input, Output> = (input: Input) => Output;
+export type AsyncProcedure<Input, Output> = Procedure<Input, Promise<Output>>;
+export type ProcedureKey = string;
+
+export type SearchResultItem = {
+  key: ProcedureKey;
+  description: string;
+  relavence: number;
+};
+
+export type SearchResult = {
+  items: SearchResultItem[];
+  query: string;
+};
+
+export type Procbase = {
+  search: (query: string) => Promise<SearchResult>;
+  write: (code: string) => Promise<ProcedureKey>;
+  read: (key: ProcedureKey) => Promise<string>;
+}
+
+export const makeProcbase = (path: string) => {
+
+}
